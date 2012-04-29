87a8aec95a75661abd2d6514bf41837bff45a186
Robert Böhnke <robb@robb.is>
2012-04-29 22:01:42 +0200

    Initial commit

---
+++ Underscore/Underscore.h
@@ -0,0 +1,9 @@
+//
+//  Underscore.h
+//  Underscore
+//
+//  Created by Robert Böhnke on 4/29/12.
+//  Copyright (c) 2012 Robert Böhnke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ UnderscoreTests/UnderscoreTests.h
@@ -0,0 +1,13 @@
+//
+//  UnderscoreTests.h
+//  UnderscoreTests
+//
+//  Created by Robert Böhnke on 4/29/12.
+//  Copyright (c) 2012 Robert Böhnke. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnderscoreTests : SenTestCase
+
+@end
