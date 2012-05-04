479caa557af380a0e17f5950c73c8651eddf6bee
Robert Böhnke <robb@robb.is>
2012-05-04 23:45:58 +0200

    Adding methods declarations to NSDictionary+Underscore

--- Underscore/NSDictionary+Underscore.h
+++ Underscore/NSDictionary+Underscore.h
@@ -8,6 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^UnderscoreDictionaryIteratorBlock)(id key, id obj);
+typedef id   (^UnderscoreDictionaryMapBlock)(id key, id obj);
+typedef BOOL (^UnderscoreDictionaryTestBlock)(id key, id obj);
+typedef id   (^UnderscoreDictionaryReduceBlock)(id memo, id key, id obj);
+
 @interface NSDictionary (Underscore)
+
+- (NSDictionary *)__prune;
+
+- (id)__reduce:(UnderscoreDictionaryReduceBlock)block intialValue:(id)memo;
+
+- (void)__each:(UnderscoreDictionaryIteratorBlock)block;
+
+- (NSDictionary *)__map:(UnderscoreDictionaryMapBlock)block;
+
+- (id)__find:(UnderscoreDictionaryTestBlock)block;
+
+- (NSArray *)__filter:(UnderscoreDictionaryTestBlock)block;
+- (NSArray *)__reject:(UnderscoreDictionaryTestBlock)block;
+
+- (BOOL)__all:(UnderscoreDictionaryTestBlock)block;
+- (BOOL)__any:(UnderscoreDictionaryTestBlock)block;
 
 @end
--- Underscore/NSDictionary+Underscore.m
+++ Underscore/NSDictionary+Underscore.m
@@ -10,4 +10,5 @@
 
 @implementation NSDictionary (Underscore)
 
+
 @end
