6b8c5477fc38265146c203e79d2fd4375dd1ac92
Robert Böhnke <robb@robb.is>
2013-05-07 12:36:55 +0200

    Adding USDictionary protocol

---
+++ Underscore/USDictionary.h
@@ -0,0 +1,32 @@
+//
+//  USDictionary.h
+//  Underscore
+//
+//  Created by Robert Böhnke on 5/7/13.
+//  Copyright (c) 2013 Robert Böhnke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "USArray.h"
+
+@protocol USDictionary <NSObject>
+
+@property (readonly) NSArray<USArray> *keys;
+@property (readonly) NSArray<USArray> *values;
+
+@property (readonly) NSDictionary<USDictionary> *(^each)(UnderscoreDictionaryIteratorBlock block);
+@property (readonly) NSDictionary<USDictionary> *(^map)(UnderscoreDictionaryMapBlock block);
+
+@property (readonly) NSDictionary<USDictionary> *(^pick)(NSArray *keys);
+
+@property (readonly) NSDictionary<USDictionary> *(^extend)(NSDictionary *source);
+@property (readonly) NSDictionary<USDictionary> *(^defaults)(NSDictionary *defaults);
+
+@property (readonly) NSDictionary<USDictionary> *(^filterKeys)(UnderscoreTestBlock block);
+@property (readonly) NSDictionary<USDictionary> *(^filterValues)(UnderscoreTestBlock block);
+
+@property (readonly) NSDictionary<USDictionary> *(^rejectKeys)(UnderscoreTestBlock block);
+@property (readonly) NSDictionary<USDictionary> *(^rejectValues)(UnderscoreTestBlock block);
+
+@end
