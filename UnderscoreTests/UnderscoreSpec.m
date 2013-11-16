276100679a95de1caca5fbcbb5c6a3b3239819b5
Robert Böhnke <robb@robb.is>
2013-11-16 14:36:09 +0100

    Add specta and expecta tests

---
+++ UnderscoreTests/UnderscoreSpec.m
@@ -0,0 +1,17 @@
+//
+//  UnderscoreSpec.m
+//  Underscore
+//
+//  Created by Robert Böhnke on 16/11/13.
+//  Copyright (c) 2013 Robert Böhnke. All rights reserved.
+//
+
+#import "Underscore.h"
+
+SpecBegin(Underscore)
+
+it(@"should have a lot of specs", ^{
+    expect(@YES).to.beTruthy();
+});
+
+SpecEnd
