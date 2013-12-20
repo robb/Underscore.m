1b76b2b07db0be57b8ba93fc12b097e6f831e278
Vasco Orey <vasco.orey@gmail.com>
2013-12-20 20:57:31 +0000

    Removed UnderscoreSpec.m. Inlined strings into test cases for better readability.

--- UnderscoreTests/Strings/UnderscoreStringsSpec.m
+++ UnderscoreTests/Strings/UnderscoreStringsSpec.m
@@ -8,14 +8,6 @@
 
 #import "Underscore+Strings.h"
 
-static NSString *stringWithLeadingWhitespace = @" underscore strings will be pretty useful";
-static NSString *stringWithTrailingWhitespace = @"underscore strings will be pretty useful ";
-static NSString *stringWithoutWhitespace = @"underscorestringswillbeprettyuseful";
-static NSString *goodString = @"underscore strings will be pretty useful";
-static NSString *onlyWhitespaceString = @"                               ";
-static NSString *trimmedWhitespaceString = @"                             ";
-static NSString *emptyString = @"";
-
 SpecBegin(UnderscoreStrings)
 
 describe(@"trim: get rid of unneccessary whitespace characters.", ^{
@@ -24,32 +16,35 @@ describe(@"trim: get rid of unneccessary whitespace characters.", ^{
         expect(Underscore.stringTrim(nil)).to.beNil;
     });
     
+    it(@"should work with empty strings", ^{
+        expect(Underscore.string(@"").trim().unwrap).to.equal(@"");
+        expect(Underscore.stringTrim(@"")).to.equal(@"");
+    });
+    
     it(@"should work with leading whitespace", ^{
-        expect(Underscore.string(stringWithLeadingWhitespace).trim().unwrap).to.equal(goodString);
-        expect(Underscore.stringTrim(stringWithLeadingWhitespace)).to.equal(goodString);
+        expect(Underscore.string(@" underscore strings will be pretty useful").trim().unwrap).to.equal(@"underscore strings will be pretty useful");
+        expect(Underscore.stringTrim(@" underscore strings will be pretty useful")).to.equal(@"underscore strings will be pretty useful");
     });
     
     it(@"should work with trailing whitespace", ^{
-        expect(Underscore.string(stringWithTrailingWhitespace).trim().unwrap).to.equal(goodString);
-        expect(Underscore.stringTrim(stringWithTrailingWhitespace)).to.equal(goodString);
+        expect(Underscore.string(@"underscore strings will be pretty useful ").trim().unwrap).to.equal(@"underscore strings will be pretty useful");
+        expect(Underscore.stringTrim(@"underscore strings will be pretty useful ")).to.equal(@"underscore strings will be pretty useful");
     });
     
     it(@"should work without any whitespace", ^{
-        expect(Underscore.string(stringWithoutWhitespace).trim().unwrap).to.equal(stringWithoutWhitespace);
-        expect(Underscore.stringTrim(stringWithoutWhitespace)).to.equal(stringWithoutWhitespace);
+        expect(Underscore.string(@"underscore").trim().unwrap).to.equal(@"underscore");
+        expect(Underscore.stringTrim(@"underscore")).to.equal(@"underscore");
     });
     
     it(@"should work with only whitespace", ^{
-        expect(Underscore.string(onlyWhitespaceString).trim().unwrap).to.equal(trimmedWhitespaceString);
-        expect(Underscore.stringTrim(onlyWhitespaceString)).to.equal(trimmedWhitespaceString);
+        expect(Underscore.string(@"   ").trim().unwrap).to.equal(@" ");
+        expect(Underscore.stringTrim(@"   ")).to.equal(@" ");
+        expect(Underscore.string(@"  ").trim().unwrap).to.equal(@"");
+        expect(Underscore.stringTrim(@"  ")).to.equal(@"");
+        expect(Underscore.string(@" ").trim().unwrap).to.equal(@"");
+        expect(Underscore.stringTrim(@" ")).to.equal(@"");
     });
 });
-
-static NSString *uncapitalized = @"hello";
-static NSString *allCaps = @"HELLO";
-static NSString *capitalized = @"Hello";
-static NSString *multipleUncaps = @"hello there";
-static NSString *multipleCapitalized = @"Hello There";
 
 describe(@"capitalize: uppercase the first letter of a string.", ^{
     it(@"should behave with nil", ^{
@@ -57,24 +52,29 @@ describe(@"capitalize: uppercase the first letter of a string.", ^{
         expect(Underscore.stringCapitalize(nil)).to.beNil;
     });
     
+    it(@"should work with empty strings", ^{
+        expect(Underscore.string(@"").capitalize().unwrap).to.equal(@"");
+        expect(Underscore.stringCapitalize(@"")).to.equal(@"");
+    });
+    
     it(@"should behave with lowercase strings", ^{
-        expect(Underscore.string(uncapitalized).capitalize().unwrap).to.equal(capitalized);
-        expect(Underscore.stringCapitalize(uncapitalized)).to.equal(capitalized);
+        expect(Underscore.string(@"hello").capitalize().unwrap).to.equal(@"Hello");
+        expect(Underscore.stringCapitalize(@"hello")).to.equal(@"Hello");
     });
     
     it(@"should behave with capitalized strings", ^{
-        expect(Underscore.string(capitalized).capitalize().unwrap).to.equal(capitalized);
-        expect(Underscore.stringCapitalize(capitalized)).to.equal(capitalized);
+        expect(Underscore.string(@"Hello There").capitalize().unwrap).to.equal(@"Hello There");
+        expect(Underscore.stringCapitalize(@"Hello There")).to.equal(@"Hello There");
     });
     
     it(@"should behave with uppercase strings", ^{
-        expect(Underscore.string(allCaps).capitalize().unwrap).to.equal(capitalized);
-        expect(Underscore.stringCapitalize(allCaps)).to.equal(capitalized);
+        expect(Underscore.string(@"HELLO THERE").capitalize().unwrap).to.equal(@"Hello There");
+        expect(Underscore.stringCapitalize(@"HELLO THERE")).to.equal(@"Hello There");
     });
     
     it(@"should keep whitespace intact", ^{
-        expect(@(Underscore.string(stringWithLeadingWhitespace).capitalize().unwrap.length)).to.equal(@(stringWithLeadingWhitespace.length));
-        expect(@(Underscore.stringCapitalize(stringWithLeadingWhitespace).length)).to.equal(@(stringWithLeadingWhitespace.length));
+        expect(Underscore.string(@"       HELLO THERE How are you").capitalize().unwrap).to.equal(@"       Hello There How Are You");
+        expect(Underscore.stringCapitalize(@"       HELLO THERE How are you")).to.equal(@"       Hello There How Are You");
     });
 });
 
@@ -84,24 +84,29 @@ describe(@"lowercase: make all characters in the string lowercase", ^{
         expect(Underscore.stringLowercase(nil)).to.beNil;
     });
     
+    it(@"should work with empty strings", ^{
+        expect(Underscore.string(@"").lowercase().unwrap).to.equal(@"");
+        expect(Underscore.stringLowercase(@"")).to.equal(@"");
+    });
+    
     it(@"should behave with lowercase strings", ^{
-        expect(Underscore.string(uncapitalized).lowercase().unwrap).to.equal(uncapitalized);
-        expect(Underscore.stringLowercase(uncapitalized)).to.equal(uncapitalized);
+        expect(Underscore.string(@"hello").lowercase().unwrap).to.equal(@"hello");
+        expect(Underscore.stringLowercase(@"hello")).to.equal(@"hello");
     });
     
     it(@"should behave with capitalized strings", ^{
-        expect(Underscore.string(capitalized).lowercase().unwrap).to.equal(uncapitalized);
-        expect(Underscore.stringLowercase(capitalized)).to.equal(uncapitalized);
+        expect(Underscore.string(@"Hello There").lowercase().unwrap).to.equal(@"hello there");
+        expect(Underscore.stringLowercase(@"Hello There")).to.equal(@"hello there");
     });
     
     it(@"should behave with uppercase strings", ^{
-        expect(Underscore.string(allCaps).lowercase().unwrap).to.equal(uncapitalized);
-        expect(Underscore.stringLowercase(allCaps)).to.equal(uncapitalized);
+        expect(Underscore.string(@"HELLO THERE").lowercase().unwrap).to.equal(@"hello there");
+        expect(Underscore.stringLowercase(@"HELLO THERE")).to.equal(@"hello there");
     });
     
     it(@"should keep whitespace intact", ^{
-        expect(@(Underscore.string(stringWithLeadingWhitespace).lowercase().unwrap.length)).to.equal(@(stringWithLeadingWhitespace.length));
-        expect(@(Underscore.stringLowercase(stringWithLeadingWhitespace).length)).to.equal(@(stringWithLeadingWhitespace.length));
+        expect(Underscore.string(@"       HELLO THERE How are you").lowercase().unwrap).to.equal(@"       hello there how are you");
+        expect(Underscore.stringLowercase(@"       HELLO THERE How are you")).to.equal(@"       hello there how are you");
     });
 });
 
@@ -111,24 +116,29 @@ describe(@"uppercase: make all characters in the string uppercase", ^{
         expect(Underscore.stringUppercase(nil)).to.beNil;
     });
     
+    it(@"should work with empty strings", ^{
+        expect(Underscore.string(@"").uppercase().unwrap).to.equal(@"");
+        expect(Underscore.stringUppercase(@"")).to.equal(@"");
+    });
+    
     it(@"should behave with lowercase strings", ^{
-        expect(Underscore.string(uncapitalized).uppercase().unwrap).to.equal(allCaps);
-        expect(Underscore.stringUppercase(uncapitalized)).to.equal(allCaps);
+        expect(Underscore.string(@"hello").uppercase().unwrap).to.equal(@"HELLO");
+        expect(Underscore.stringUppercase(@"hello")).to.equal(@"HELLO");
     });
     
     it(@"should behave with capitalized strings", ^{
-        expect(Underscore.string(capitalized).uppercase().unwrap).to.equal(allCaps);
-        expect(Underscore.stringUppercase(capitalized)).to.equal(allCaps);
+        expect(Underscore.string(@"Hello There").uppercase().unwrap).to.equal(@"HELLO THERE");
+        expect(Underscore.stringUppercase(@"Hello There")).to.equal(@"HELLO THERE");
     });
     
     it(@"should behave with uppercase strings", ^{
-        expect(Underscore.string(allCaps).uppercase().unwrap).to.equal(allCaps);
-        expect(Underscore.stringUppercase(allCaps)).to.equal(allCaps);
+        expect(Underscore.string(@"HELLO THERE").uppercase().unwrap).to.equal(@"HELLO THERE");
+        expect(Underscore.stringUppercase(@"HELLO THERE")).to.equal(@"HELLO THERE");
     });
     
     it(@"should keep whitespace intact", ^{
-        expect(@(Underscore.string(stringWithLeadingWhitespace).uppercase().unwrap.length)).to.equal(@(stringWithLeadingWhitespace.length));
-        expect(@(Underscore.stringUppercase(stringWithLeadingWhitespace).length)).to.equal(@(stringWithLeadingWhitespace.length));
+        expect(Underscore.string(@"       HELLO THERE How are you").uppercase().unwrap).to.equal(@"       HELLO THERE HOW ARE YOU");
+        expect(Underscore.stringUppercase(@"       HELLO THERE How are you")).to.equal(@"       HELLO THERE HOW ARE YOU");
     });
 });
 
